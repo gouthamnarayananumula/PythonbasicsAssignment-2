22143e763106041a2892b6c1d460549d5064edab
gouthamnarayananumula <144488067+gouthamnarayananumula@users.noreply.github.com>
2023-09-18 21:32:32 +0530

    Add files via upload

---
+++ Python assignment basics-2.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Question-1
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+a>b
+
+
+# In[2]:
+
+
+# Question-2 AND operator
+a=True and True
+b=True and False
+c=False and True
+d=False and False
+print(a,b,c,d)
+
+
+# In[3]:
+
+
+# Question-2 OR operator
+a=True or True
+b=False or True
+c=True or False
+d=False or False
+print(a,b,c,d)
+
+
+# In[4]:
+
+
+# Question-2 NOT operator
+a=not True
+b=not False
+print(a,b)
+
+
+# In[7]:
+
+
+# Question-4 
+print(5>4 and 3==5)
+print(not(5>4))
+print(5>4 or 3==5)
+print(not(5>4 or 3==5))
+print((True and True) and (True==False))
+print((not False)or(not True))
+
+
+# In[8]:
+
+
+# Question-11
+for i in range(10):
+    print(i)
+
+
+# In[10]:
+
+
+# Question-11
+for i in range(0,10):
+    print(i)
+
+
+# In[12]:
+
+
+# Question-11
+for i in range(0,10,1):
+    print(i)
+
+
+# In[13]:
+
+
+# Question-12 Using For loop
+for i in range(1,11):
+    print(i)
+
+
+# In[15]:
+
+
+# Question-12 Using While loop
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+
+# In[ ]:
+
+
+
+
